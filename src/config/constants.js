9b215b286980f85b82091a6eeb3d3cffa9cc8b29
ilycode2@gmail.com <70672465+IlhamAbdul07@users.noreply.github.com>
2026-04-16 20:39:51 +0700

    forms lagi

--- src/config/constants.js
+++ src/config/constants.js
@@ -9,7 +9,7 @@ export const BRAND_DOMAIN = 'ilyschool.com'
 export const WHATSAPP_NUMBER = import.meta.env.VITE_WA_NUMBER || '6285178226071'
 export const GFORM_URL =
   import.meta.env.VITE_GFORM_URL ||
-  'https://forms.gle/https://forms.gle/1uBfy1Hj3ci7Vj6E9'
+  'https://forms.gle/1uBfy1Hj3ci7Vj6E9'
 export const EMAIL = import.meta.env.VITE_EMAIL || 'ilycode2@gmail.com'
 
 // SOCIAL MEDIA
